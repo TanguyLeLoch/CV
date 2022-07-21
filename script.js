c781f3f81a8958da5146199da845576c3533b771
Tanguy Le Loch <lelochtanguy@gmail.com>
2022-07-21 02:54:23 +0200

    date styled

---
+++ script.js
@@ -0,0 +1,13 @@
+// get element by class name with query selector
+const uls = document.querySelectorAll('.stack');
+const separator = document.createElement('li');
+separator.classList.add('separator');
+for (let i = 0; i < uls.length; i++) {
+    const ul = uls[i];
+    const lis = ul.querySelectorAll('li');
+    for (let j = 1; j < lis.length; j++) {
+        const li = lis[j];
+        console.log(li);
+        li.before(separator.cloneNode(true));
+    }
+}
