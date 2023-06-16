58ebf9cb757240bbcfe230f7c21245d9ce0d7cb2
Tanguy Le Loch <lelochtanguy@gmail.com>
2023-06-16 12:33:14 +0700

    add familiar tools

--- script.js
+++ script.js
@@ -10,3 +10,36 @@ for (let i = 0; i < uls.length; i++) {
         li.before(separator.cloneNode(true));
     }
 }
+
+// set '/' in blue color
+const familiarTools = document.querySelectorAll('.familiar-tools');
+for  (let i = 0; i < familiarTools.length; i++) {
+    const familiarTool = familiarTools[i];
+    const text = familiarTool.textContent;
+
+    let  tools = text.split('/');
+    // trim each tool
+    tools = tools.map(item => item.trim());
+    // set hidden in css
+    familiarTool.hidden = true;
+    let toolsUpdated = document.createElement("span");
+
+    toolsUpdated.classList.add('familiar-tools');
+    for (let j = 0; j < tools.length; j++) {
+        const tool = tools[j];
+        const span = document.createElement('span');
+        span.textContent = tool;
+        toolsUpdated.appendChild(span);
+        if (j < tools.length - 1) {
+            const slash = document.createElement('span');
+            slash.textContent = ' / ';
+            slash.style.color = 'rgb(36, 163, 242)';
+            slash.style.fontWeight = 'bold';
+            toolsUpdated.appendChild(slash);
+        }
+    }
+    familiarTool.after(toolsUpdated);
+
+
+    console.log(tools)
+}
